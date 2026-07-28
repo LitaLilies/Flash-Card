adc6a03ccc2d44ea0bbd0f29a49f977250831bf9
LitaLilies <dttduyenduyen7807@gmail.com>
2026-07-28 09:43:51 +0700

    update color console

---
+++ ui.h
@@ -0,0 +1,26 @@
+#ifndef UI_H
+#define UI_H
+
+#include "flashcard.h"
+
+// Ma mau console
+enum ConsoleColor {
+    CLR_BLACK        = 0,
+    CLR_BLUE         = 1,
+    CLR_GREEN        = 2,
+    CLR_CYAN         = 3,
+    CLR_RED          = 4,
+    CLR_MAGENTA      = 5,
+    CLR_YELLOW       = 6,
+    CLR_WHITE        = 7,
+    CLR_GRAY         = 8,
+    CLR_LIGHT_BLUE   = 9,
+    CLR_LIGHT_GREEN  = 10,
+    CLR_LIGHT_CYAN   = 11,
+    CLR_LIGHT_RED    = 12,
+    CLR_LIGHT_MAGENTA= 13,
+    CLR_LIGHT_YELLOW = 14,
+    CLR_BRIGHT_WHITE = 15
+};
+
+#endif // UI_H
