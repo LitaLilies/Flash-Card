86c25f5ae5bac2fc66a5828616445f117f2dad56
trang050907-code <trang050907@gmail.com>
2026-07-30 00:28:47 +0700

    update flashcard.h phần 5.File i/o

--- flashcard.h
+++ flashcard.h
@@ -7,26 +7,35 @@ using namespace std;
 // =====================================================================
 //  4. SINGLY LINKED LIST - Lich su kiem tra
 // =====================================================================
-struct HistoryNode {
+struct HistoryNode
+{
     string date;
     string topic;
     int correct;
     int total;
-    HistoryNode* next;
+    HistoryNode *next;
 };
 
-struct HistoryList {
-    HistoryNode* head;
+struct HistoryList
+{
+    HistoryNode *head;
     int count;
 };
 
-void initHistory(HistoryList& list);
+void initHistory(HistoryList &list);
 // Them vao dau (moi nhat o dau)
-void addHistory(HistoryList& list, const string& date, const string& topic, int correct, int total);
+void addHistory(HistoryList &list, const string &date, const string &topic, int correct, int total);
 // Xoa 1 muc theo STT (1-based)
-void deleteHistory(HistoryList& list, int index);
+void deleteHistory(HistoryList &list, int index);
 // Xoa toan bo lich su
-void clearHistory(HistoryList& list);
-void freeHistory(HistoryList& list);
+void clearHistory(HistoryList &list);
+void freeHistory(HistoryList &list);
 
-#endif // FLASHCARD_H
+// =====================================================================
+//  5. FILE I/O
+// =====================================================================
+void loadTopics(const string &filename, TopicNode *&root);
+void saveTopics(const string &filename, TopicNode *root);
+
+void loadCards(const string &filename, CardList &list);
+void saveCards(const string &filename, CardList &list);
